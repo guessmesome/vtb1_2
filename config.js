218af86da28ad15f6f906bbd23aec77490788e1a
nn <205666415+guessmesome@users.noreply.github.com>
2025-11-13 19:38:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const CONFIG = {
     products: {
         1: 'https://trnsfx.ru/go/sj2exjfhw7',
         2: 'https://trnsfx.ru/go/sehws6w3c5',
-        3: 'https://trnsfx.ru/go/s7jg4se9q6',
-        4: 'https://trnsfx.ru/go/skzm0vozgw'
+        3: 'https://trnsfx.ru/go/skzm0vozgw',
+        4: 'https://trnsfx.ru/go/s7jg4se9q6'
     }
 };
